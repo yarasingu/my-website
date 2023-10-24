633db3dd7652a33209487c43c75e72a6601f4061
yarisingutarun@gmail.com <yarisingutarun@gmail.com>
2023-10-24 07:36:03 +0000

    Edited signup_module

--- client_code/signup_module.py
+++ client_code/signup_module.py
@@ -4,8 +4,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 def generate_user_id():
-  full_table = app_tables.amigos.search()
-  for row in full_table:
-    print(row['amigos_id'])
+  full_table = app_tables.amigos.search(tables.order_by("amigos_id" ,ascending=False))
+  print(full_table["amigos_id"][0])
+
 
 generate_user_id()
